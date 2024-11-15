b02943dbd64134d7a7992fa01ea3e994decf8414
abuiliazeed <abuiliazeed@gmail.com>
2024-11-15 21:22:14 +0200

    prod

--- backend/main.py
+++ backend/main.py
@@ -37,7 +37,7 @@ app = FastAPI(
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://todo-front-zeta-eight.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
